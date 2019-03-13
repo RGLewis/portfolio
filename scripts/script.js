3571048aead752a1eb8e10694b3d6682a28db4a9
Rafaela <rafi.lewis@me.com>
2019-03-13 16:47:38 -0400

    got the header and clip path working

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+// Granim
+var granimInstance = new Granim({
+    element: '#canvas-basic',
+    direction: 'left-right',
+    transitionSpeed: 1000,
+    isPausedWhenNotInView: true,
+    states: {
+        "default-state": {
+            gradients: [
+                ['#F5F5F5', '#5CDB95'],
+                ['#B0E0E6', '#F5F5F5'],
+                ['#FDFDFD', '#b3d6f9']
+            ]
+        }
+    }
+});
+
+var granimInstance = new Granim({
+    element: '#canvas-dip-bottom',
+    direction: 'left-right',
+    transitionSpeed: 1000,
+    isPausedWhenNotInView: true,
+    states: {
+        "default-state": {
+            gradients: [
+                ['#F5F5F5', '#5CDB95'],
+                ['#B0E0E6', '#F5F5F5'],
+                ['#FDFDFD', '#b3d6f9']
+            ]
+        }
+    }
+});
