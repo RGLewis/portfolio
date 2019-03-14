9636de25927cc336373d1a28a1ddf17459ab4436
Rafaela <rafi.lewis@me.com>
2019-03-13 21:43:40 -0400

    nav animation in

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,10 @@
-// Granim
-var granimInstance = new Granim({
-    element: '#canvas-basic',
-    direction: 'left-right',
-    transitionSpeed: 1000,
-    isPausedWhenNotInView: true,
-    states: {
-        "default-state": {
-            gradients: [
-                ['#F5F5F5', '#5CDB95'],
-                ['#B0E0E6', '#F5F5F5'],
-                ['#FDFDFD', '#b3d6f9']
-            ]
-        }
-    }
-});
 
-var granimInstance = new Granim({
-    element: '#canvas-dip-bottom',
-    direction: 'left-right',
-    transitionSpeed: 1000,
-    isPausedWhenNotInView: true,
-    states: {
-        "default-state": {
-            gradients: [
-                ['#F5F5F5', '#5CDB95'],
-                ['#B0E0E6', '#F5F5F5'],
-                ['#FDFDFD', '#b3d6f9']
-            ]
-        }
-    }
+$(function(){
+    // Handle Show Nav
+    $(".menu-button").on("click", function () {
+        // $(".nav-container").toggleClass("show-nav-container");
+        // $(".nav-button-container").toggleClass("show-nav-button-container");
+        $(".nav-list").toggleClass("show-nav-list");
+    });
+
 });
