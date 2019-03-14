7255f21e4a0634d0185643b40aefcd2825ba7e4c
Rafaela <rafi.lewis@me.com>
2019-03-14 12:50:54 -0400

    got the SVG animationnnnnnnn

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,6 @@ AOS.init({
     duration: 1000,
 })
 
-// Anime Js
-// anime({
-//     targets: 'div',
-//     translateX: 250,
-//     rotate: '1turn',
-//     backgroundColor: '#FFF',
-//     duration: 800
-// });
-
 $(function(){
     // Handle Show Nav
     $(".menu-button").on("click", function () {
